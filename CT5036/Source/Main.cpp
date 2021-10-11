7c7541cdc7d1f7e3ea1458148ad62fcae8d8d7c4
HER <Sylveon2112@gmail.com>
2021-10-11 01:23:55 +0100

    implemented GLFW library, struggling with file pathways? will ask in class

---
+++ CT5036/Source/Main.cpp
@@ -0,0 +1,36 @@
+#include <GLFW/glfw3.h>
+
+int main()
+{
+	//Initialise GLFW
+	if (!glfwInit())
+	{
+		return -1;
+	}
+
+	//create a windowed mode window and it's OpenGL context
+	GLFWwindow* window;
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	//make the window's context current
+	glfwMakeContextCurrent(window);
+
+	//loop until the user closes the window
+	while (!glfwWindowShouldClose(window))
+	{
+		//draw code goes here
+
+		//swap front and back buffers
+		glfwSwapBuffers(window);
+
+		//poll for and process events
+		glfwPollEvents();
+	}
+	glfwTerminate();
+	return 0;
+}
